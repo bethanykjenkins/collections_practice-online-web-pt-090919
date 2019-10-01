18721d5e83588b69ecc5f664e0fda6aa7fa3179e
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-01 15:21:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array_integers)
+  array_integers.sort
+end
 
+def sort_array_desc(array_integers)
+  array_integers.sort.reverse
+end
+
+def sort_array_char_count(array_strings)
+  array_strings.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array (integer_array)
+  integer_array.reverse
+end
